ecef47dc22fc62c3ec6a6f1f0ed8f159f26c155e
STDOK <stevedok@gmail.com>
2021-10-14 13:54:25 +0300

    commit

---
+++ workshops/calc.js
@@ -0,0 +1,618 @@
+let answers = [{
+        "Answers": [
+            "rec0HoIcvrQZhenrY",
+            "rec57fdGiIwVIspSC",
+            "rec9VAyWE1nADxZIa",
+            "recEzJUpNC5jrzoyW",
+            "recHSGqs7JlhcOp5n"
+        ],
+        "Type": "other",
+        "User": [
+            "rec0fA7UTVpVXdmPw"
+        ],
+        "Name": "21, 8, 23, 20, 18",
+        "id": "recFWNiKCfigmNDRV",
+        "createdTime": "2021-10-03T07:57:25.000Z",
+        "__IMTLENGTH__": 7,
+        "__IMTINDEX__": 1
+    },
+    {
+        "Answers": [
+            "rec0Axy7seBQklUT2",
+            "rec0HoIcvrQZhenrY",
+            "rec3L1RaXFMI6mgnv",
+            "rec57fdGiIwVIspSC",
+            "rec9VAyWE1nADxZIa"
+        ],
+        "User": [
+            "recrZMCkJ8MWOi61p"
+        ],
+        "Type": "other",
+
+        "Name": "John Doe 2",
+        "id": "recKsqRLtr35CH9hP",
+        "createdTime": "2021-10-03T08:14:09.000Z",
+        "__IMTLENGTH__": 7,
+        "__IMTINDEX__": 2
+    },
+    {
+        "Answers": [
+            "rec0Axy7seBQklUT2",
+            "rec0HoIcvrQZhenrY",
+            "rec3L1RaXFMI6mgnv",
+            "rec57fdGiIwVIspSC",
+            "rec9VAyWE1nADxZIa"
+        ],
+        "User": [
+            "recrZMCkJ8MWOi61p"
+        ],
+        "Type": "other",
+
+        "Name": "John Doe 2",
+        "id": "recS4Mt4CyUoSItK5",
+        "createdTime": "2021-10-03T08:14:06.000Z",
+        "__IMTLENGTH__": 7,
+        "__IMTINDEX__": 3
+    },
+    {
+        "Answers": [
+            "rec0Axy7seBQklUT2",
+            "rec0HoIcvrQZhenrY",
+            "rec3L1RaXFMI6mgnv",
+            "rec57fdGiIwVIspSC",
+            "rec9VAyWE1nADxZIa"
+        ],
+        "Type": "self",
+        "User": [
+            "rec0fA7UTVpVXdmPw"
+        ],
+        "Name": "28, 21, 27, 8, 23",
+        "id": "recWQKnLoYkvsYG5n",
+        "createdTime": "2021-10-03T07:51:48.000Z",
+        "__IMTLENGTH__": 7,
+        "__IMTINDEX__": 4
+    },
+    {
+        "Answers": [
+            "rec0Axy7seBQklUT2",
+            "rec3L1RaXFMI6mgnv",
+            "recUIV4jcnVb9rUDk",
+            "recXRRT1R1AcJTDPa",
+            "recXVmnJlLPkFS3MH"
+        ],
+        "Type": "other",
+        "User": [
+            "rec0fA7UTVpVXdmPw"
+        ],
+        "Name": "28, 27, 11, 15, 24",
+        "id": "rechFAzAx6YLhJOFQ",
+        "createdTime": "2021-10-03T07:57:36.000Z",
+        "__IMTLENGTH__": 7,
+        "__IMTINDEX__": 5
+    },
+    {
+        "Answers": [
+            "rec0Axy7seBQklUT2",
+            "rec0HoIcvrQZhenrY",
+            "rec3L1RaXFMI6mgnv",
+            "rec57fdGiIwVIspSC",
+            "rec9VAyWE1nADxZIa"
+        ],
+        "User": [
+            "rec0fA7UTVpVXdmPw"
+        ],
+        "Name": "Stepan Doktor",
+        "Type": "other",
+        "id": "reckSZcFBCwca9I2X",
+        "createdTime": "2021-10-03T08:14:01.000Z",
+        "__IMTLENGTH__": 7,
+        "__IMTINDEX__": 6
+    },
+    {
+        "Answers": [
+            "rec0Axy7seBQklUT2",
+            "rec3L1RaXFMI6mgnv",
+            "recUIV4jcnVb9rUDk",
+            "recXRRT1R1AcJTDPa",
+            "recXVmnJlLPkFS3MH"
+        ],
+        "User": [
+            "reckgv9XqzUp5ut0Z"
+        ],
+        "Type": "other",
+
+        "Name": "28, 27, 11, 15, 24",
+        "id": "recmcMS7bYdscMKiX",
+        "createdTime": "2021-10-03T08:13:36.000Z",
+        "__IMTLENGTH__": 7,
+        "__IMTINDEX__": 7
+    }
+]
+
+const questions = [
+    {
+        "Answers": [
+            "recWQKnLoYkvsYG5n",
+            "rechFAzAx6YLhJOFQ",
+            "recmcMS7bYdscMKiX",
+            "reckSZcFBCwca9I2X",
+            "recS4Mt4CyUoSItK5",
+            "recKsqRLtr35CH9hP",
+            "recDAYHaIGn7REsqS"
+        ],
+        "Self": "You are not 'too kind'",
+        "Category": "Congruence",
+        "Others": "Is not 'too kind'",
+        "Title": "28",
+        "function": "rec0Axy7seBQklUT2",
+        "id": "rec0Axy7seBQklUT2",
+        "createdTime": "2021-10-03T07:23:17.000Z",
+        "__IMTLENGTH__": 28,
+        "__IMTINDEX__": 1
+    },
+    {
+        "Answers": [
+            "recWQKnLoYkvsYG5n",
+            "recFWNiKCfigmNDRV",
+            "reckSZcFBCwca9I2X",
+            "recS4Mt4CyUoSItK5",
+            "recKsqRLtr35CH9hP",
+            "recDAYHaIGn7REsqS"
+        ],
+        "Self": "You tell people how you feel; what you think and believe",
+        "Category": "Openness",
+        "Others": "Tells people how he / she feels; what he / she thinks and believes",
+        "Title": "21",
+        "function": "rec0HoIcvrQZhenrY",
+        "id": "rec0HoIcvrQZhenrY",
+        "createdTime": "2021-10-03T07:23:17.000Z",
+        "__IMTLENGTH__": 28,
+        "__IMTINDEX__": 2
+    },
+    {
+        "Answers": [
+            "recWQKnLoYkvsYG5n",
+            "rechFAzAx6YLhJOFQ",
+            "recmcMS7bYdscMKiX",
+            "reckSZcFBCwca9I2X",
+            "recS4Mt4CyUoSItK5",
+            "recKsqRLtr35CH9hP",
+            "recDAYHaIGn7REsqS"
+        ],
+        "Self": "You 'walk the talk'",
+        "Category": "Congruence",
+        "Others": "Walks the talk'",
+        "Title": "27",
+        "function": "rec3L1RaXFMI6mgnv",
+        "id": "rec3L1RaXFMI6mgnv",
+        "createdTime": "2021-10-03T07:23:17.000Z",
+        "__IMTLENGTH__": 28,
+        "__IMTINDEX__": 3
+    },
+    {
+        "Answers": [
+            "recWQKnLoYkvsYG5n",
+            "recFWNiKCfigmNDRV",
+            "reckSZcFBCwca9I2X",
+            "recS4Mt4CyUoSItK5",
+            "recKsqRLtr35CH9hP",
+            "recDAYHaIGn7REsqS"
+        ],
+        "Self": "You respect others' point of view",
+        "Category": "Acceptance",
+        "Others": "Respects others' point of view",
+        "Title": "8",
+        "function": "rec57fdGiIwVIspSC",
+        "id": "rec57fdGiIwVIspSC",
+        "createdTime": "2021-10-03T07:23:17.000Z",
+        "__IMTLENGTH__": 28,
+        "__IMTINDEX__": 4
+    },
+    {
+        "Answers": [
+            "recWQKnLoYkvsYG5n",
+            "recFWNiKCfigmNDRV",
+            "reckSZcFBCwca9I2X",
+            "recS4Mt4CyUoSItK5",
+            "recKsqRLtr35CH9hP",
+            "recDAYHaIGn7REsqS"
+        ],
+        "Self": "You don't play games or have a hidden agenda",
+        "Category": "Congruence",
+        "Others": "Doesn't play games or have a hidden agenda",
+        "Title": "23",
+        "function": "rec9VAyWE1nADxZIa",
+        "id": "rec9VAyWE1nADxZIa",
+        "createdTime": "2021-10-03T07:23:17.000Z",
+        "__IMTLENGTH__": 28,
+        "__IMTINDEX__": 5
+    },
+    {
+        "Self": "You are objective",
+        "Category": "Reliability",
+        "Others": "Is objective",
+        "Title": "3",
+        "function": "recBvR5afeXuixRfo",
+        "id": "recBvR5afeXuixRfo",
+        "createdTime": "2021-10-03T06:57:06.000Z",
+        "__IMTLENGTH__": 28,
+        "__IMTINDEX__": 6
+    },
+    {
+        "Answers": [
+            "recFWNiKCfigmNDRV"
+        ],
+        "Self": "You are straight forward",
+        "Category": "Openness",
+        "Others": "Is straight forward",
+        "Title": "20",
+        "function": "recEzJUpNC5jrzoyW",
+        "id": "recEzJUpNC5jrzoyW",
+        "createdTime": "2021-10-03T07:23:17.000Z",
+        "__IMTLENGTH__": 28,
+        "__IMTINDEX__": 7
+    },
+    {
+        "Answers": [
+            "recFWNiKCfigmNDRV"
+        ],
+        "Self": "You use body language that matches words and behavior",
+        "Category": "Openness",
+        "Others": "Uses body language that matches words and behavior",
+        "Title": "18",
+        "function": "recHSGqs7JlhcOp5n",
+        "id": "recHSGqs7JlhcOp5n",
+        "createdTime": "2021-10-03T07:23:17.000Z",
+        "__IMTLENGTH__": 28,
+        "__IMTINDEX__": 8
+    },
+    {
+        "Self": "You are on time, meet deadlines and deliver",
+        "Category": "Reliability",
+        "Others": "Is on time, meet deadlines and deliver",
+        "Title": "1",
+        "function": "recJ5Xoiadqxsx9Ln",
+        "id": "recJ5Xoiadqxsx9Ln",
+        "createdTime": "2021-10-03T06:57:06.000Z",
+        "__IMTLENGTH__": 28,
+        "__IMTINDEX__": 9
+    },
+    {
+        "Self": "You are empathetic",
+        "Category": "Acceptance",
+        "Others": "Is empathetic",
+        "Title": "9",
+        "function": "recLtocBJ5ViZvJB1",
+        "id": "recLtocBJ5ViZvJB1",
+        "createdTime": "2021-10-03T07:23:17.000Z",
+        "__IMTLENGTH__": 28,
+        "__IMTINDEX__": 10
+    },
+    {
+        "Self": "You keep promises",
+        "Category": "Reliability",
+        "Others": "Keeps promises",
+        "Title": "5",
+        "function": "recNvyIGuaChKzMzY",
+        "id": "recNvyIGuaChKzMzY",
+        "createdTime": "2021-10-03T07:23:17.000Z",
+        "__IMTLENGTH__": 28,
+        "__IMTINDEX__": 11
+    },
+    {
+        "Self": "You are thorough and accurate in your facts",
+        "Category": "Reliability",
+        "Others": "Is thorough and accurate in your facts",
+        "Title": "4",
+        "function": "recPpI2s5FdlXr7ja",
+        "id": "recPpI2s5FdlXr7ja",
+        "createdTime": "2021-10-03T07:23:17.000Z",
+        "__IMTLENGTH__": 28,
+        "__IMTINDEX__": 12
+    },
+    {
+        "Answers": [
+            "rechFAzAx6YLhJOFQ",
+            "recmcMS7bYdscMKiX"
+        ],
+        "Self": "You listen",
+        "Category": "Acceptance",
+        "Others": "Listens",
+        "Title": "11",
+        "function": "recUIV4jcnVb9rUDk",
+        "id": "recUIV4jcnVb9rUDk",
+        "createdTime": "2021-10-03T07:23:17.000Z",
+        "__IMTLENGTH__": 28,
+        "__IMTINDEX__": 13
+    },
+    {
+        "Answers": [
+            "rechFAzAx6YLhJOFQ",
+            "recmcMS7bYdscMKiX"
+        ],
+        "Self": "You clarify expectations",
+        "Category": "Openness",
+        "Others": "Clarifies expectations",
+        "Title": "15",
+        "function": "recXRRT1R1AcJTDPa",
+        "id": "recXRRT1R1AcJTDPa",
+        "createdTime": "2021-10-03T07:23:17.000Z",
+        "__IMTLENGTH__": 28,
+        "__IMTINDEX__": 14
+    },
+    {
+        "Answers": [
+            "rechFAzAx6YLhJOFQ",
+            "recmcMS7bYdscMKiX"
+        ],
+        "Self": "You are sincere",
+        "Category": "Congruence",
+        "Others": "Is sincere",
+        "Title": "24",
+        "function": "recXVmnJlLPkFS3MH",
+        "id": "recXVmnJlLPkFS3MH",
+        "createdTime": "2021-10-03T07:23:17.000Z",
+        "__IMTLENGTH__": 28,
+        "__IMTINDEX__": 15
+    },
+    {
+        "Self": "You make rules clear",
+        "Category": "Congruence",
+        "Others": "Makes rules clear",
+        "Title": "26",
+        "function": "recYBzI7ONMerJbsw",
+        "id": "recYBzI7ONMerJbsw",
+        "createdTime": "2021-10-03T07:23:17.000Z",
+        "__IMTLENGTH__": 28,
+        "__IMTINDEX__": 16
+    },
+    {
+        "Self": "You take action",
+        "Category": "Reliability",
+        "Others": "Takes action",
+        "Title": "6",
+        "function": "recZjfbHZmPDsgxuq",
+        "id": "recZjfbHZmPDsgxuq",
+        "createdTime": "2021-10-03T07:23:17.000Z",
+        "__IMTLENGTH__": 28,
+        "__IMTINDEX__": 17
+    },
+    {
+        "Self": "You set boundaries",
+        "Category": "Congruence",
+        "Others": "Sets boundaries",
+        "Title": "25",
+        "function": "recagyJ77588cYvqe",
+        "id": "recagyJ77588cYvqe",
+        "createdTime": "2021-10-03T07:23:17.000Z",
+        "__IMTLENGTH__": 28,
+        "__IMTINDEX__": 18
+    },
+    {
+        "Self": "You challenge others on important issues",
+        "Category": "Reliability",
+        "Others": "Challenges others on important issues",
+        "Title": "7",
+        "function": "recf8vHL6y1fKFhiG",
+        "id": "recf8vHL6y1fKFhiG",
+        "createdTime": "2021-10-03T07:23:17.000Z",
+        "__IMTLENGTH__": 28,
+        "__IMTINDEX__": 19
+    },
+    {
+        "Self": "You are non-judgmental about people",
+        "Category": "Acceptance",
+        "Others": "Is non-judgmental about people",
+        "Title": "10",
+        "function": "reclwgq5mWA4SfIqL",
+        "id": "reclwgq5mWA4SfIqL",
+        "createdTime": "2021-10-03T07:23:17.000Z",
+        "__IMTLENGTH__": 28,
+        "__IMTINDEX__": 20
+    },
+    {
+        "Self": "You don't criticize",
+        "Category": "Openness",
+        "Others": "Doesn't criticize",
+        "Title": "17",
+        "function": "recm0ha7Ibfucp0RN",
+        "id": "recm0ha7Ibfucp0RN",
+        "createdTime": "2021-10-03T07:23:17.000Z",
+        "__IMTLENGTH__": 28,
+        "__IMTINDEX__": 21
+    },
+    {
+        "Self": "You are dependable and consistent",
+        "Category": "Reliability",
+        "Others": "Is dependable and consistent",
+        "Title": "2",
+        "function": "recpO8kckG5le4OC0",
+        "id": "recpO8kckG5le4OC0",
+        "createdTime": "2021-10-03T06:57:06.000Z",
+        "__IMTLENGTH__": 28,
+        "__IMTINDEX__": 22
+    },
+    {
+        "Self": "You allow mistakes",
+        "Category": "Acceptance",
+        "Others": "Allows mistakes",
+        "Title": "12",
+        "function": "recr2fkS97cdIt3gk",
+        "id": "recr2fkS97cdIt3gk",
+        "createdTime": "2021-10-03T07:23:17.000Z",
+        "__IMTLENGTH__": 28,
+        "__IMTINDEX__": 23
+    },
+    {
+        "Self": "You value opinions of others and are willing to hear what they say",
+        "Category": "Openness",
+        "Others": "Values opinions of others and is willing to hear what they say",
+        "Title": "16",
+        "function": "recrSkzDBmaZZ5kuq",
+        "id": "recrSkzDBmaZZ5kuq",
+        "createdTime": "2021-10-03T07:23:17.000Z",
+        "__IMTLENGTH__": 28,
+        "__IMTINDEX__": 24
+    },
+    {
+        "Self": "You talk straight",
+        "Category": "Congruence",
+        "Others": "Talks straight",
+        "Title": "22",
+        "function": "recu4Isz7zCrdO5JT",
+        "id": "recu4Isz7zCrdO5JT",
+        "createdTime": "2021-10-03T07:23:17.000Z",
+        "__IMTLENGTH__": 28,
+        "__IMTINDEX__": 25
+    },
+    {
+        "Self": "You accept limitations",
+        "Category": "Acceptance",
+        "Others": "Accepts limitations",
+        "Title": "13",
+        "function": "recu6Z1iJBmboEMFs",
+        "id": "recu6Z1iJBmboEMFs",
+        "createdTime": "2021-10-03T07:23:17.000Z",
+        "__IMTLENGTH__": 28,
+        "__IMTINDEX__": 26
+    },
+    {
+        "Self": "You are honest about limitations",
+        "Category": "Openness",
+        "Others": "Is honest about limitations",
+        "Title": "19",
+        "function": "recud5mQIHqb4WpqY",
+        "id": "recud5mQIHqb4WpqY",
+        "createdTime": "2021-10-03T07:23:17.000Z",
+        "__IMTLENGTH__": 28,
+        "__IMTINDEX__": 27
+    },
+    {
+        "Self": "You don't try to change people",
+        "Category": "Acceptance",
+        "Others": "Doesn't try to change people",
+        "Title": "14",
+        "function": "recz0YGK1kMriK1Nq",
+        "id": "recz0YGK1kMriK1Nq",
+        "createdTime": "2021-10-03T07:23:17.000Z",
+        "__IMTLENGTH__": 28,
+        "__IMTINDEX__": 28
+    }
+]
+
+const questionLits =questions.map(item=>{return item.id})
+
+answers.forEach(it => {
+    it._user = it.User[0]
+    delete it.__IMTLENGTH__
+    delete it.__IMTINDEX__
+    delete it.createdTime
+})
+function findQuestion(id){
+    let found = questions.find(item=>{return item.id===id})
+    return found.Self
+}
+var groupBy = function (xs, key) {
+    return xs.reduce(function (rv, x) {
+        (rv[x[key]] = rv[x[key]] || []).push(x);
+        return rv;
+    }, {});
+};
+
+let byUser = groupBy(answers, "_user")
+let usersAr = Object.keys(byUser)
+
+usersAr.forEach(it => {
+
+    byUser[it].forEach(item => {
+        delete item.id
+        delete item._user
+        delete item.Name
+        delete item.User
+    })
+
+    byUser[it] = groupBy(byUser[it], 'Type')
+
+
+    if (byUser[it].other !== undefined) {
+        let temp = byUser[it].other.map(transform).flat()
+        byUser[it].other = temp
+
+
+    }
+    if (byUser[it].self !== undefined) {
+        let temp = byUser[it].self.map(transform).flat()
+        byUser[it].self = temp
+
+    }
+
+    byUser[it].questions = {}
+    questionLits.map(item => {
+        let self = 0
+        let other = 0
+        if (byUser[it].self !== undefined) {
+
+            let search = byUser[it].self.filter(search => {
+                return search === item
+            })
+            self = search.length
+        }
+        if (byUser[it].other !== undefined) {
+
+            let search = byUser[it].other.filter(search => {
+                return search === item
+            })
+            other = search.length
+        }
+
+        byUser[it].questions[item] = {
+            self,
+            other
+        }
+    })
+
+
+    let resultArray = {
+        Arena: [],
+        Hidden: [],
+        Blind_spot: [],
+        Unknown: []
+    }
+
+    let qa = byUser[it].questions
+    Object.keys(qa).forEach(item => {
+        
+        if (qa[item].self === 0 && qa[item].other === 0) {
+            resultArray.Unknown.push(findQuestion(item))
+
+        } else if (qa[item].self > 0 && qa[item].other === 0) {
+            resultArray.Hidden.push(findQuestion(item))
+
+        } else if (qa[item].self === 0 && qa[item].other > 0) {
+            resultArray.Blind_spot.push(findQuestion(item))
+
+        } else if (qa[item].self > 0 && qa[item].other > 0) {
+            resultArray.Arena.push(findQuestion(item))
+        }
+
+    })
+byUser[it].resultArray = resultArray
+})
+
+let resultArray = {
+    Arena: [],
+    Hidden: [],
+    Blind_spot: [],
+    Unknown: []
+}
+
+
+function transform(an) {
+    delete an.Type
+    an = an.Answers
+   return an
+}
+
+
+let text = byUser['rec0fA7UTVpVXdmPw'].resultArray
+console.log(JSON.stringify(text))
